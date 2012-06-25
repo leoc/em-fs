e47e52f56b3901cbe4f8e6f1d4de0231d589ea2f
Arthur Andersen <leoc.git@gmail.com>
2012-06-25 15:25:21 +0200

    `EM::Dir` only provides `#glob` for now

--- lib/em-fs/dir.rb
+++ lib/em-fs/dir.rb
@@ -1,33 +1,14 @@
 require 'em-fs/dir/glob'
-require 'em-fs/dir/listing'
 
 module EventMachine
   class Dir
     class << self
 
-
-      def each expr, options = {}, &block
-
+      def glob pattern
+        EM::Dir::Glob.new pattern
       end
+      alias :[] :glob
 
-      ##
-      # Executes a block for each entry for a given directory with the
-      # basename as parameter. (Look at `EM::Dir.each` for additional
-      # options)
-      def each_entry expr, options = {}, &block
-        options.merge! depth: 0
-        each expr, options do |stat|
-          block.call stat.filename
-        end
-      end
-
-      ##
-      # Executes a block for each entry for a given directory with the
-      # full path for that entry as parameter. (Look at `EM::Dir.each`
-      # for additional options)
-      def each_path expr, options = {}
-
-      end
     end
   end
 end
--- spec/dir/listing_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe EM::Dir::Listing do
-
-end
