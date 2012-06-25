ecbcb1e799168c4dbc86e5f46756bd3433e64ad4
Arthur Andersen <leoc.git@gmail.com>
2012-06-25 11:36:12 +0200

    implemented simple specs for `EM::FS`

--- spec/fs_spec.rb
+++ spec/fs_spec.rb
@@ -2,11 +2,27 @@ require 'spec_helper'
 
 describe EM::FS do
   describe '.rsync' do
-
+    it 'should create an `RsyncCommand` with :progress flag' do
+      EM.run do
+        @dir = File.join(SPEC_ROOT, 'data', 'test')
+        cmd = EM::FS.rsync '/usr/share/test1', '/usr/share/test2'
+        cmd.should be_a EM::FS::RsyncCommand
+        cmd.command.should == 'rsync --progress /usr/share/test1 /usr/share/test2'
+        EM.stop_event_loop
+      end
+    end
   end
 
   describe '.find' do
-
+    it 'should create `SystemCommand`' do
+      EM.run do
+        @dir = File.join(SPEC_ROOT, 'data', 'test')
+        cmd = EM::FS.find '.'
+        cmd.should be_a EM::SystemCommand
+        cmd.command.should == 'find .'
+        EM.stop_event_loop
+      end
+    end
   end
 
 end
