50a4d742de7be592ee3b78d56621632ef7ee1fc5
Arthur Andersen <leoc.git@gmail.com>
2012-07-04 11:03:33 +0200

    changed dir_spec a little

--- spec/dir_spec.rb
+++ spec/dir_spec.rb
@@ -3,30 +3,14 @@ require 'spec_helper'
 
 describe EM::Dir do
   describe '.[]' do
-    it 'should create a `DirListing` instance'
-  end
-
-  describe '#each' do
-    context 'with depth = 0' do
-      it 'should execute the block for each entry'
-    end
-
-    context 'with depth = 1' do
-      it 'should execute the block for each entry and directory´s entry'
-    end
-
-    context 'with depth = :inf' do
-      it 'should execute the block for each entry in tree'
+    it 'should create a `EM::Dir::Glob` instance' do
+      EM::Dir['./**/*.rb'].should be_a EM::Dir::Glob
     end
   end
 
-  describe '#each_entry' do
-    it 'should execute a block with entry for each entry'
-    it 'should use depth = 0 by default'
-  end
-
-  describe '#each_path' do
-    it 'should execute a block with full path for each entry'
-    it 'should use depth = :inf by default'
+  describe '.glob' do
+    it 'should create a `EM::Dir::Glob` instance' do
+      EM::Dir['./**/*.rb'].should be_a EM::Dir::Glob
+    end
   end
 end
