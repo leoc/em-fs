60885d5f7c5429f858f3fdf6abdf5009a261c931
Arthur Andersen <leoc.git@gmail.com>
2012-07-06 17:27:18 +0200

    implemented Dir glob with specs

--- lib/em-fs/dir/glob.rb
+++ lib/em-fs/dir/glob.rb
@@ -2,6 +2,80 @@ module EventMachine
   class Dir
     class Glob
 
+      FORMAT = "%m %D '%y' %G %n %i '%p' %s %U %A@ %T@ %C@\\n"
+
+      def initialize pattern
+        @weight = nil
+        parse pattern
+      end
+
+      def parse pattern
+        root = []
+
+        *path, name = pattern.split(::File::SEPARATOR)
+
+        if name.index("*").nil?
+          path << name
+          name = "*"
+        end
+
+        root << path.shift while path[0] and path[0].index("*").nil?
+
+        @name = name
+        @path = path.join(::File::SEPARATOR).gsub '**', '*'
+        @root = root.join(::File::SEPARATOR)
+
+        if path.length == 0
+          @weight = 1
+        end
+
+        @root = '.' if @root == ''
+      end
+
+      def each options = {}, &block
+        options = {
+          depth: :inf
+        }.merge options
+        EM::SystemCommand.execute find_command(options) do |on|
+          on.stdout.line do |line|
+            block.call File::Stat.parse line
+          end
+        end
+      end
+
+      def each_entry options = {}, &block
+        options = {
+          depth: 1
+        }.merge options
+        each options do |stat|
+          block.call ::File.basename(stat.path)
+        end
+      end
+
+      def each_path options = {}, &block
+        options = {
+          depth: :inf
+        }.merge options
+        each options do |stat|
+          block.call stat.path
+        end
+      end
+
+      private
+      def find_command options = {}
+        options = {
+          depth: (@weight || :inf)
+        }.merge options
+
+        builder = EM::SystemCommand::Builder.new 'find'
+        builder << @root
+        builder << [ :path, @path ] unless @path == "*" or @path == ''
+        builder << [ :name, @name ] unless @name == "*"
+        builder << [ :maxdepth, options[:depth] ] unless options[:depth] == :inf
+        builder << [ :printf, FORMAT ]
+        builder.to_s.gsub(/-+(\w)/, "-\\1")
+      end
+
     end
   end
 end
--- spec/dir/glob_spec.rb
+++ spec/dir/glob_spec.rb
@@ -1,5 +1,186 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe EM::Dir::Glob do
+  before :all do
+    dirs = [ 'a', 'b/a', 'b/b', 'c/a/a', 'c/a/b' ].map { |p|
+      File.join(SPEC_ROOT, 'data', p)
+    }
+    files = [ 'a/x', 'b/x', 'b/a/x', 'b/a/y', 'b/a/z' ].map { |p|
+      File.join(SPEC_ROOT, 'data', p)
+    }
+    FileUtils.touch files
+    FileUtils.mkdir_p dirs
+  end
 
+  describe '#find_command' do
+    it 'should parse relative pattern' do
+      cmd = EM::Dir['./*.rb'].send :find_command
+      cmd.should == "find . -name '*.rb' -maxdepth 1 -printf '%m %D '\\''%y'\\'' %G %n %i '\\''%p'\\'' %s %U %A@ %T@ %C@\\n'"
+    end
+
+    it 'should parse relative pattern with path wildcard' do
+      cmd = EM::Dir['./**/*.rb'].send :find_command
+      cmd.should == "find . -name '*.rb' -printf '%m %D '\\''%y'\\'' %G %n %i '\\''%p'\\'' %s %U %A@ %T@ %C@\\n'"
+    end
+  end
+
+  describe '#each' do
+    context 'with depth = 0' do
+      before :all do
+        @entries = []
+        pattern = File.join(SPEC_ROOT, 'data', '**', '*')
+        EM.run do
+          cmd = EM::Dir[pattern].each depth: 1 do |entry|
+            @entries << entry.path
+          end
+
+          cmd.exit do
+            EM.stop_event_loop
+          end
+        end
+      end
+
+      it 'should execute the block for each entry' do
+        @entries.should == [
+                            "/home/arthur/projects/em-fs/spec/data",
+                            "/home/arthur/projects/em-fs/spec/data/b",
+                            "/home/arthur/projects/em-fs/spec/data/c",
+                            "/home/arthur/projects/em-fs/spec/data/test2",
+                            "/home/arthur/projects/em-fs/spec/data/test",
+                            "/home/arthur/projects/em-fs/spec/data/a",
+                            "/home/arthur/projects/em-fs/spec/data/test3"
+                           ]
+      end
+    end
+
+    context 'with depth = 2' do
+      before :all do
+        @entries = []
+        pattern = File.join(SPEC_ROOT, 'data', '**', '*')
+        EM.run do
+          cmd = EM::Dir[pattern].each depth: 2 do |entry|
+            @entries << entry.path
+          end
+
+          cmd.exit do
+            EM.stop_event_loop
+          end
+        end
+      end
+
+      it 'should execute the block for each entry and directory´s entry' do
+        @entries.should == [
+                            "/home/arthur/projects/em-fs/spec/data",
+                            "/home/arthur/projects/em-fs/spec/data/b",
+                            "/home/arthur/projects/em-fs/spec/data/b/b",
+                            "/home/arthur/projects/em-fs/spec/data/b/x",
+                            "/home/arthur/projects/em-fs/spec/data/b/a",
+                            "/home/arthur/projects/em-fs/spec/data/c",
+                            "/home/arthur/projects/em-fs/spec/data/c/a",
+                            "/home/arthur/projects/em-fs/spec/data/test2",
+                            "/home/arthur/projects/em-fs/spec/data/test",
+                            "/home/arthur/projects/em-fs/spec/data/a",
+                            "/home/arthur/projects/em-fs/spec/data/a/x",
+                            "/home/arthur/projects/em-fs/spec/data/test3"
+                           ]
+      end
+    end
+
+    context 'with depth = :inf' do
+      before :all do
+        @entries = []
+        pattern = File.join(SPEC_ROOT, 'data', '**', '*')
+        EM.run do
+          cmd = EM::Dir[pattern].each depth: :inf do |entry|
+            @entries << entry.path
+          end
+
+          cmd.exit do
+            EM.stop_event_loop
+          end
+        end
+      end
+
+      it 'should execute the block for each entry in tree' do
+        @entries.should == [
+                            "/home/arthur/projects/em-fs/spec/data",
+                            "/home/arthur/projects/em-fs/spec/data/b",
+                            "/home/arthur/projects/em-fs/spec/data/b/b",
+                            "/home/arthur/projects/em-fs/spec/data/b/x",
+                            "/home/arthur/projects/em-fs/spec/data/b/a",
+                            "/home/arthur/projects/em-fs/spec/data/b/a/x",
+                            "/home/arthur/projects/em-fs/spec/data/b/a/z",
+                            "/home/arthur/projects/em-fs/spec/data/b/a/y",
+                            "/home/arthur/projects/em-fs/spec/data/c",
+                            "/home/arthur/projects/em-fs/spec/data/c/a",
+                            "/home/arthur/projects/em-fs/spec/data/c/a/b",
+                            "/home/arthur/projects/em-fs/spec/data/c/a/a",
+                            "/home/arthur/projects/em-fs/spec/data/test2",
+                            "/home/arthur/projects/em-fs/spec/data/test",
+                            "/home/arthur/projects/em-fs/spec/data/a",
+                            "/home/arthur/projects/em-fs/spec/data/a/x",
+                            "/home/arthur/projects/em-fs/spec/data/test3"
+                           ]
+      end
+    end
+  end
+
+  describe '#each_entry' do
+    before :all do
+      @entries = []
+      pattern = File.join(SPEC_ROOT, 'data', '**', '*')
+      EM.run do
+        cmd = EM::Dir[pattern].each_entry do |entry|
+          @entries << entry
+        end
+
+        cmd.exit do
+          EM.stop_event_loop
+        end
+      end
+    end
+
+    it 'should execute a block with file name for each entry' do
+        @entries.should == [ "data", "b", "c", "test2", "test", "a", "test3" ]
+      end
+  end
+
+  describe '#each_path' do
+    before :all do
+      @entries = []
+      pattern = File.join(SPEC_ROOT, 'data', '**', '*')
+      EM.run do
+        cmd = EM::Dir[pattern].each_path do |path|
+          @entries << path
+        end
+
+        cmd.exit do
+          EM.stop_event_loop
+        end
+      end
+    end
+
+    it 'should execute a block with full path for each entry' do
+        @entries.should == [
+                            "/home/arthur/projects/em-fs/spec/data",
+                            "/home/arthur/projects/em-fs/spec/data/b",
+                            "/home/arthur/projects/em-fs/spec/data/b/b",
+                            "/home/arthur/projects/em-fs/spec/data/b/x",
+                            "/home/arthur/projects/em-fs/spec/data/b/a",
+                            "/home/arthur/projects/em-fs/spec/data/b/a/x",
+                            "/home/arthur/projects/em-fs/spec/data/b/a/z",
+                            "/home/arthur/projects/em-fs/spec/data/b/a/y",
+                            "/home/arthur/projects/em-fs/spec/data/c",
+                            "/home/arthur/projects/em-fs/spec/data/c/a",
+                            "/home/arthur/projects/em-fs/spec/data/c/a/b",
+                            "/home/arthur/projects/em-fs/spec/data/c/a/a",
+                            "/home/arthur/projects/em-fs/spec/data/test2",
+                            "/home/arthur/projects/em-fs/spec/data/test",
+                            "/home/arthur/projects/em-fs/spec/data/a",
+                            "/home/arthur/projects/em-fs/spec/data/a/x",
+                            "/home/arthur/projects/em-fs/spec/data/test3"
+                           ]
+      end
+  end
 end
